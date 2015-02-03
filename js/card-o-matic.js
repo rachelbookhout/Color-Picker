6cba47855aeae217ccfce44989a2cca416c79fa2
rachelbookhout <rbookhou@gmail.com>
2015-02-03 18:47:44 -0500

    textures change colors

--- js/card-o-matic.js
+++ js/card-o-matic.js
@@ -4,11 +4,17 @@ $('.colors').click(function() {
   var color_that_was_clicked = $(this).css('background-color');
 
   // Set the canvas background to be that color...
-  $('#canvas').css('background-color',color_that_was_clicked);
+  $('#canvas, .textures').css('background-color',color_that_was_clicked);
+
+  //$('.textures').css('background-color',color_that_was_clicked);
+
 
 });
 
 $('.textures').click(function() {
-  var texture_clicked = $(this).css('background-image');
-  $('#canvas').css('background-image',texture_clicked);
+
+  var texture_that_was_clicked = $(this).css('background-image');
+
+  $('#canvas').css('background-image',texture_that_was_clicked);
+
 });
